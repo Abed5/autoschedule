06d95c8d532528d761719feec14cf72bdcf0d1a5
Abednego Kipkirui <abed@Flow-mac.local>
2020-08-20 04:09:32 -0500

    calendar IDs and timezone details

---
+++ autoschedule/config.py
@@ -0,0 +1,9 @@
+#stubs - calendar in which you have blank time blocks
+#schedule - calendar in which to schedule tasks
+#time_Zone - find list of timezones in timeZones.json
+calendar_IDs = {
+    'stubs' : 'email_1',
+    'schedule' : 'email_2'
+}
+
+time_Zone = "Etc/GMT"
