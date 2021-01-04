047178d3c45b978482bf3816734f7ad21239989c
abed5 <abednegokipkirui2021@u.northwestern.edu>
2021-01-04 15:16:04 +0300

    comments

--- autoschedule/generate_tasks.py
+++ autoschedule/generate_tasks.py
@@ -1,17 +1,27 @@
 def read_file():
+    # Read lines from tasks_layout.txt
     with open("tasks_layout.txt", "r") as b:
         k = b.readlines()[3:]
     return k
 
 def remove_formatting(List):
+    # Remove formatting from the list of tasks
     m = [i.replace('\n', '').split(' ') for i in List if '#' not in i]
     return m
 
 def strings_to_ints(List):
+    # Converts strings to integers (some parameters, to be specified)
     l = [[int(j[0]), j[1], int(j[2])] for j in List]
     return l
 
 def ints_to_weight(List):
+
+    # convert integer at end of line to weight
+    # 1 = 4
+    # 2 = 2
+    # 4 = 1
+    # else: encountered a non-conforming weight
+
     a = List
     b = []
     for i in a:
@@ -28,6 +38,7 @@ def ints_to_weight(List):
     return b
 
 def tasks_with_weights(List):
+    # generate tasks with weights
     a = List
     b = []
     for i in a:
@@ -37,12 +48,14 @@ def tasks_with_weights(List):
 
 
 def weight_of_group(group):
+    # return total weight of a group of tasks
     a = 0
     for i in group:
         a += i[0]
     return a
 
 def weight_less(group):
+    # return True if weight of group is less than 4 otherwise False
     b = weight_of_group(group)
     if b < 4:
         return True
@@ -51,6 +64,7 @@ def weight_less(group):
 
 
 def loadable(group, task):
+    # return False if a group is filled (total weight == 4) otherwise True
     if weight_of_group(group) + task[0] <= 4:
         return True
     else:
@@ -58,6 +72,7 @@ def loadable(group, task):
 
 
 def group_to_weight4(List):
+    # group tasks into 4-weight list
     a = List
     b = [[]]
     for i in a:
@@ -82,6 +97,7 @@ def group_to_weight4(List):
 
 
 def shuffle_tasks(my_tasks):
+    # Shuffle and group tasks
     m = copy.deepcopy(my_tasks)
     undecided = True
     for i in m:
@@ -106,6 +122,7 @@ def shuffle_tasks(my_tasks):
 
 
 def write_tasks_to_file(my_tasks, date_today):
+    # Write tasks to tasks_list.txt
     with open("tasks_list.txt", "w") as my_file:
         my_file.writelines('#Each task > 2 letters\n' + '20' + date_today.strftime("%y") + ', '+ date_today.strftime("%m") + ' , ' + date_today.strftime("%d") + '\n')
         k = 1
